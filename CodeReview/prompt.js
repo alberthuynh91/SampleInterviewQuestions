e166d14413acc0eae494562e33c8ac79fc2e55db
Albert Huynh <ahuynh@fanatics.com>
2021-08-11 11:24:17 -0700

    Add code review

---
+++ CodeReview/prompt.js
@@ -0,0 +1,7 @@
+// Look at the codereview.png
+// 
+// If you were doing a code review of codereview.png and are looking at line 27, 
+// what would be a comment you would potentially leave for the user regarding this line of code?
+//
+// Answer:
+//
