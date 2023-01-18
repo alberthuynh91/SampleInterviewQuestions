e79773670799fb6f9fdbf7f052068c83ffb2580f
Albert Huynh <alberthuynh91@gmail.com>
2023-01-18 10:40:58 -0800

    Delete Javascript/Promises3 directory

--- Javascript/Promises3/prompt.js
+++
@@ -1,5 +0,0 @@
-// Implement a clone of the native Javascript Promise class called MyPromise
-// The class should contain a .then method
-// The class should contain a .catch method
-// The class should contain a .finally method
-// MyPromise should be able to chain .then 
